e0ccc407ee3fc684704b02f93535b8a1a6c563af
ToyamaHideki <toyama.hideki.bns@gmail.com>
2026-01-10 13:58:25 +0900

    Spring Initializrでひな形作成
    以下依存関係を設定
    MyBatis Framework: SQL中心の開発。
    
    PostgreSQL Driver: DB接続用。
    
    Spring Web: Web機能の基盤。
    
    Lombok: コードの簡略化。
    
    Validation: 入力チェックの自動化。
    
    Spring Boot DevTools: 保存時の自動再起動。
    
    Docker Compose Support: アプリ起動時にDBも自動起動（★開発スピードUP）。
    
    Spring Configuration Processor: 設定ファイルの補完。

---
+++ src/main/java/com/toyama/bookkeeping/BookkeepingAppApplication.java
@@ -0,0 +1,13 @@
+package com.toyama.bookkeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkeepingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkeepingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyama/bookkeeping/BookkeepingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyama.bookkeeping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkeepingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
